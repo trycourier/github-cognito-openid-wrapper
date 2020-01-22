53d78454c5b461c5aaf5670e6403a49528a0e396
Danny Douglass <266481+DannyDouglass@users.noreply.github.com>
2020-01-22 14:39:07 -0800

    ga-wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"  # Figure out where the
 PROJECT_ROOT="$SCRIPT_DIR"/.. # Figure out where the project directory is
 
 # Ensure dependencies are present
+
 require_binary aws
 require_binary sam
 
