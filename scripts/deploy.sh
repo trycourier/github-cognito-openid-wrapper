2831682ca7aee4077410585bd8d78490b9487e14
Danny Douglass <266481+DannyDouglass@users.noreply.github.com>
2020-03-26 12:07:03 -0700

    remove kms key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,7 @@ aws s3 mb "s3://$BUCKET_NAME" --region "$REGION" || true
 echo "packaged successfully!"
 
 # note if encryption is turned on in s3, you will also need to pass in --kms-key-id below:
-sam package --template-file template.yml --output-template-file "$OUTPUT_TEMPLATE_FILE"  --s3-bucket "$BUCKET_NAME"
+sam package --debug --template-file template.yml --output-template-file "$OUTPUT_TEMPLATE_FILE" --s3-bucket "$BUCKET_NAME"
 
 sam deploy \
   --capabilities CAPABILITY_IAM \
